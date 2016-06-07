3d96420147e1d250bfe0546dba4e8bdfdc8fb229
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-06 23:08:15 -0500

    partial de progressbar

---
+++ resources/views/partials/progressbar.blade.php
@@ -0,0 +1,9 @@
+<div class="col-md-12">
+    <div class="form-group">
+        <div id="progressbar" class="progress progress-striped active">
+            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+                <span class="sr-only"> 100% Completo </span>
+            </div>
+        </div>
+    </div>
+</div>
