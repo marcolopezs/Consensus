a6cf1f3e5258b78011d2732240b6fbdc24e69c62
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-09 03:45:56 -0500

    cambio de nombre de Tareas a Procesos

--- resources/views/system/expediente/tareas/list.blade.php
+++ resources/views/system/expediente/tareas/list.blade.php
@@ -1,6 +1,6 @@
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-    <h4 class="modal-title">Tareas</h4>
+    <h4 class="modal-title">Procesos</h4>
 </div>
 <div class="modal-body">
     <div class="row">
