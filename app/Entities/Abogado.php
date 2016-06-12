5fbaedd62071975a9c86260faf08d6800ccb359e
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-12 03:58:47 -0500

    ocultando campos de fecha

--- app/Entities/Abogado.php
+++ app/Entities/Abogado.php
@@ -9,5 +9,6 @@ class Abogado extends BaseEntity {
     protected $dates = ['deleted_at'];
 
     protected $fillable = ['cliente','dni','ruc','carnet_extranjeria','pasaporte','partidad_nacimiento','otros','email','telefono','fax','direccion','pais_id'];
+    protected $hidden = ['created_at','updated_at','deleted_at'];
 
 }
