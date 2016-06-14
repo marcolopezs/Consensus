f8b5f4fdf4fe1b3d36a1ac1d9da8a0ab61cae78d
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-14 15:43:01 -0500

    funcion para busqueda de registros en area

--- app/Repositories/AreaRepo.php
+++ app/Repositories/AreaRepo.php
@@ -1,11 +1,23 @@
 <?php namespace Consensus\Repositories;
 
 use Consensus\Entities\Area;
+use Illuminate\Http\Request;
 
 class AreaRepo extends BaseRepo {
 
     public function getModel()
     {
         return new Area();
+    }
+
+    //BUSQUEDA DE REGISTROS POR TITULO Y ESTADO y ORDENARLO POR SELECCION DEL USUARIO
+    public function findOrder(Request $request)
+    {
+        return $this->getModel()
+                    ->titulo($request->get('titulo'))
+                    ->email($request->get('email'))
+                    ->estado($request->get('estado'))
+                    ->order($request->get('order'))
+                    ->paginate();
     }
 }
