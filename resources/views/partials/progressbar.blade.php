31bf911f3030ee2ec6c39c28be1a45ffe2a0313c
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-14 15:37:57 -0500

    cambio de etiquetas de progressbar

--- resources/views/partials/progressbar.blade.php
+++ resources/views/partials/progressbar.blade.php
@@ -1,9 +1,5 @@
-<div class="col-md-12">
-    <div class="form-group">
-        <div id="progressbar" class="progress progress-striped active">
-            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
-                <span class="sr-only"> 100% Completo </span>
-            </div>
-        </div>
+<div id="progressbar" class="progress progress-striped active" style="display: none">
+    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+        <span class="sr-only"> 100% Completo </span>
     </div>
 </div>
