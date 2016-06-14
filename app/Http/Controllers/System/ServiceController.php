1efc4039ba800fe01aec49094c92dd91e81942d0
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-14 16:11:21 -0500

    cambio de estado de Servicios

--- app/Http/Controllers/System/ServiceController.php
+++ app/Http/Controllers/System/ServiceController.php
@@ -151,4 +151,29 @@ class ServiceController extends Controller {
             ]);
         }
     }
+
+
+    /*
+     * Cambiar Estado
+     */
+    public function estado($id, Request $request)
+    {
+        //BUSCAR ID
+        $row = $this->serviceRepo->findOrFail($id);
+
+        if($row->estado == 0){ $estado = 1; }else{ $estado = 0; }
+
+        $row->estado = $estado;
+        $this->serviceRepo->update($row, $request->all());
+
+        $message = 'El registro se modificó satisfactoriamente.';
+
+        if($request->ajax())
+        {
+            return response()->json([
+                'message' => $message,
+                'estado'  => $estado
+            ]);
+        }
+    }
 }
