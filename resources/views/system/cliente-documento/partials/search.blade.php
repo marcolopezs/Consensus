830c036bf27aa3c1babb1f55cf37684749af0c86
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-17 13:35:39 -0500

    eliminacion de carpeta documentos del cliente

--- resources/views/system/cliente-documento/partials/search.blade.php
+++
@@ -1,8 +0,0 @@
-<thead>
-    <tr role="row" class="heading">
-        <th width="35%">Titulo</th>
-        <th width="30%">Descripción</th>
-        <th width="15%">Fecha subida</th>
-        <th width="15%">Acciones</th>
-    </tr>
-</thead>
