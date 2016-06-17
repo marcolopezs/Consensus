56c44e6fec937665cc147dd906348e3a9b3968e8
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-17 00:58:13 -0500

    eliminacion de carpeta de contactos de cliente

--- resources/views/system/cliente-contacto/partials/search.blade.php
+++
@@ -1,9 +0,0 @@
-<thead>
-    <tr role="row" class="heading">
-        <th width="40%">Contacto</th>
-        <th width="10%">DNI</th>
-        <th width="10%">RUC</th>
-        <th width="30%">Email</th>
-        <th width="15%">Acciones</th>
-    </tr>
-</thead>
