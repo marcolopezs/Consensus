103c8a233fbf53f53ac71e373341f465e100aeb3
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-19 21:33:24 -0500

    agregando campo id en fillable de Abogado

--- app/Entities/Abogado.php
+++ app/Entities/Abogado.php
@@ -8,7 +8,6 @@ class Abogado extends BaseEntity {
 
     protected $dates = ['deleted_at'];
 
-    protected $fillable = ['cliente','dni','ruc','carnet_extranjeria','pasaporte','partidad_nacimiento','otros','email','telefono','fax','direccion','pais_id'];
-    protected $hidden = ['created_at','updated_at','deleted_at'];
+    protected $fillable = ['id','nombre','dni','ruc','carnet_extranjeria','pasaporte','partidad_nacimiento','otros','email','telefono','fax','direccion','pais_id'];
 
 }
