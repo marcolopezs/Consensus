56cad76eb76f1da77af091b465a7412af323b34e
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-24 00:22:48 -0500

    agregando policy para acceso a usuarios

---
+++ app/Policies/SystemPolicy.php
@@ -0,0 +1,48 @@
+<?php namespace Consensus\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class SystemPolicy
+{
+    use HandlesAuthorization;
+
+    public function before($user)
+    {
+        if($user->isAdmin())
+        {
+            return true;
+        }
+    }
+
+    public function menu($user){
+        return $user->isAdmin() || $user->isAbogado();
+    }
+
+    public function clienteExpedientesHome($user)
+    {
+        return $user->isCliente();
+    }
+
+    public function create($user)
+    {
+        return $user->yesCreate();
+    }
+
+    public function update($user)
+    {
+        return $user->yesUpdate();
+    }
+
+    public function delete($user)
+    {
+        return $user->yesDelete();
+    }
+
+    public function printer($user)
+    {
+        return $user->yesPrint();
+    }
+
+
+
+}
