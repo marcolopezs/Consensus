d5a01a7f625cd5fdd6467639e5a8e673af9403e6
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-24 00:24:07 -0500

    mostrando expedientes de usuario logeado

--- app/Http/Controllers/System/HomeController.php
+++ app/Http/Controllers/System/HomeController.php
@@ -1,22 +1,49 @@
 <?php namespace Consensus\Http\Controllers\System;
 
-use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Redirector;
 use Consensus\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Gate;
+
+use Consensus\Repositories\ClienteRepo;
+use Consensus\Repositories\ExpedienteRepo;
 
 class HomeController extends Controller {
 
-    public function __construct()
-    {
+    protected $clienteRepo;
+    protected $expedienteRepo;
 
+    protected $usuario;
+    protected $cliente;
+
+    /**
+     * HomeController constructor.
+     * @param ClienteRepo $clienteRepo
+     * @param ExpedienteRepo $expedienteRepo
+     */
+    public function __construct(ClienteRepo $clienteRepo,
+                                ExpedienteRepo $expedienteRepo)
+    {
+        $this->clienteRepo = $clienteRepo;
+        $this->expedienteRepo = $expedienteRepo;
+
+        if(Gate::allows('cliente-expedientes-home')) {
+            $this->usuario = auth()->user()->cliente_id;
+            $this->cliente = $this->clienteRepo->findOrFail($this->usuario);
+        }
     }
 
 
     public function index()
     {
-        return view('system.index');
+        if(Gate::allows('cliente-expedientes-home')){
+            $expedientes = $this->cliente->expedientes()->orderBy('created_at','desc')->paginate(6);
+
+            return view('system.index', compact('expedientes'));
+        }else{
+            return view('system.index');
+        }
     }
 
 }
--- resources/views/system/index.blade.php
+++ resources/views/system/index.blade.php
@@ -6,6 +6,60 @@
 
 @section('contenido_body')
 
+    @can('cliente-expedientes-home')
 
+        <div class="row widget-row">
+
+            <div class="col-md-12">
+                <!-- Begin: life time stats -->
+                <div class="portlet light ">
+                    <div class="portlet-title">
+                        <div class="caption">
+                            <span class="caption-subject font-blue bold uppercase">Mis Últimos Expedientes</span>
+                        </div>
+                    </div>
+                    <div class="portlet-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-hover table-bordered">
+                                <thead>
+                                <tr>
+                                    <th>Expediente</th>
+                                    <th>Abogado</th>
+                                    <th>Fecha Inicio</th>
+                                    <th>Estado</th>
+                                    <th></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($expedientes as $item)
+                                    @php
+                                    $row_id = $item->id;
+                                    $row_expediente = $item->expediente;
+                                    $row_fecha = $item->exp_fecha_inicio;
+                                    $row_abogado = $item->exp_abogado;
+                                    $row_estado = $item->exp_state;
+                                    @endphp
+                                    <tr>
+                                        <td><strong>{{ $row_expediente }}</strong></td>
+                                        <td>{{ $row_abogado }}</td>
+                                        <td>{{ $row_fecha }}</td>
+                                        <td>{{ $row_estado }}</td>
+                                        <td>
+                                            <a href="@{{ route('customer.expedientes.show', $row_id) }}" class="btn btn-xs btn-default" data-target="#ajax" data-toggle="modal">
+                                                <i class="fa fa-search"></i> Ver </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- End: life time stats -->
+            </div>
+
+        </div>
+
+    @endcan
 
 @stop
