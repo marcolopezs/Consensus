5796fdee443836cf13a140737b9a77ce6f0a2c16
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-25 02:35:38 -0500

    agregando ajustes para todos los usuarios importados

--- database/seeds/AjustesTableSeeder.php
+++ database/seeds/AjustesTableSeeder.php
@@ -37,6 +37,56 @@ class AjustesTableSeeder extends Seeder
                 'user_id' => 5,
                 'contenido' => '{"ch-expediente":"1","ch-cliente":"1","ch-abogado":"1","ch-servicio":"1","ch-estado":"1"}'
             ],
+            [
+                'model' => \Consensus\Entities\Expediente::class,
+                'user_id' => 6,
+                'contenido' => '{"ch-expediente":"1","ch-cliente":"1","ch-abogado":"1","ch-servicio":"1","ch-estado":"1"}'
+            ],
+            [
+                'model' => \Consensus\Entities\Expediente::class,
+                'user_id' => 7,
+                'contenido' => '{"ch-expediente":"1","ch-cliente":"1","ch-abogado":"1","ch-servicio":"1","ch-estado":"1"}'
+            ],
+            [
+                'model' => \Consensus\Entities\Expediente::class,
+                'user_id' => 8,
+                'contenido' => '{"ch-expediente":"1","ch-cliente":"1","ch-abogado":"1","ch-servicio":"1","ch-estado":"1"}'
+            ],
+            [
+                'model' => \Consensus\Entities\Expediente::class,
+                'user_id' => 9,
+                'contenido' => '{"ch-expediente":"1","ch-cliente":"1","ch-abogado":"1","ch-servicio":"1","ch-estado":"1"}'
+            ],
+            [
+                'model' => \Consensus\Entities\Expediente::class,
+                'user_id' => 10,
+                'contenido' => '{"ch-expediente":"1","ch-cliente":"1","ch-abogado":"1","ch-servicio":"1","ch-estado":"1"}'
+            ],
+            [
+                'model' => \Consensus\Entities\Expediente::class,
+                'user_id' => 11,
+                'contenido' => '{"ch-expediente":"1","ch-cliente":"1","ch-abogado":"1","ch-servicio":"1","ch-estado":"1"}'
+            ],
+            [
+                'model' => \Consensus\Entities\Expediente::class,
+                'user_id' => 2,
+                'contenido' => '{"ch-expediente":"1","ch-cliente":"1","ch-abogado":"1","ch-servicio":"1","ch-estado":"1"}'
+            ],
+            [
+                'model' => \Consensus\Entities\Expediente::class,
+                'user_id' => 13,
+                'contenido' => '{"ch-expediente":"1","ch-cliente":"1","ch-abogado":"1","ch-servicio":"1","ch-estado":"1"}'
+            ],
+            [
+                'model' => \Consensus\Entities\Expediente::class,
+                'user_id' => 14,
+                'contenido' => '{"ch-expediente":"1","ch-cliente":"1","ch-abogado":"1","ch-servicio":"1","ch-estado":"1"}'
+            ],
+            [
+                'model' => \Consensus\Entities\Expediente::class,
+                'user_id' => 15,
+                'contenido' => '{"ch-expediente":"1","ch-cliente":"1","ch-abogado":"1","ch-servicio":"1","ch-estado":"1"}'
+            ],
         ]);
     }
 }
