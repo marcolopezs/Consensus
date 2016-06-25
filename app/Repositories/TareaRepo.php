89a47cc5812cc4bcdb5f4e365d460057dcdcfff6
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-25 02:38:13 -0500

    funcion para filtrar tareas en home y lista

--- app/Repositories/TareaRepo.php
+++ app/Repositories/TareaRepo.php
@@ -8,4 +8,25 @@ class TareaRepo extends BaseRepo {
     {
         return new Tarea();
     }
+
+    //FILTRAR TAREAS
+    public function filterPaginate()
+    {
+        return $this->getModel()
+                    ->where('abogado_id', auth()->user()->abogado_id)
+                    ->orderBy('fecha_solicitada', 'desc')
+                    ->with('expedientes','titular','concepto','abogado')
+                    ->paginate();
+    }
+
+    //FILTRAR TAREAS PARA HOME
+    public function filterHome($estado)
+    {
+        return $this->getModel()
+                    ->where('abogado_id', auth()->user()->abogado_id)
+                    ->where('estado', $estado)
+                    ->orderBy('fecha_solicitada', 'desc')
+                    ->with('expedientes','titular','concepto','abogado')
+                    ->paginate(5);
+    }
 }
