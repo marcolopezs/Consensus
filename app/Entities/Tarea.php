68e25820b713e53ff6be101d4511f4c3e9abb88b
Marco Lopez <mlopez@marostdevelopers.com>
2016-06-25 02:40:56 -0500

    agregando funciones de relacion y getters

--- app/Entities/Tarea.php
+++ app/Entities/Tarea.php
@@ -8,18 +8,28 @@ class Tarea extends BaseEntity {
 
     protected $dates = ['deleted_at'];
 
-    protected $fillable = ['expediente_id','tarea','descripcion','fecha_solicitada','fecha_vencimiento','abogado_id','estado'];
+    protected $fillable = ['expediente_id','tarea_concepto_id','descripcion','fecha_solicitada','fecha_vencimiento','titular_id','abogado_id','estado'];
     protected $hidden = ['created_at','updated_at','deleted_at'];
-    protected $appends = ['asignado','url_editar'];
+    protected $appends = ['titulo_tarea','asignado','url_editar'];
 
     public function expedientes()
     {
-        return $this->belongsTo(Expediente::class);
+        return $this->belongsTo(Expediente::class, 'expediente_id');
     }
 
     public function abogado()
     {
         return $this->belongsTo(Abogado::class);
+    }
+
+    public function titular()
+    {
+        return $this->belongsTo(User::class, 'titular_id');
+    }
+
+    public function concepto()
+    {
+        return $this->belongsTo(TareaConcepto::class, 'tarea_concepto_id');
     }
 
     public function setFechaSolicitadaAttribute($value)
@@ -40,6 +50,16 @@ class Tarea extends BaseEntity {
     public function getFechaVencimientoAttribute($value)
     {
         return soloFecha($value);
+    }
+
+    public function getAsignadoPorAttribute()
+    {
+        return $this->titular->nombre_completo;
+    }
+
+    public function getTituloTareaAttribute()
+    {
+        return $this->concepto->titulo;
     }
 
     public function getAsignadoAttribute()
