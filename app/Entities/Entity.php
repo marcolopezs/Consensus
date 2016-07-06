409ab7583ed9e771808f03cc96ae6d168327e31b
Marco Lopez <mlopez@marostdevelopers.com>
2016-07-06 18:35:44 -0500

    scopeOrder en entidad

--- app/Entities/Entity.php
+++ app/Entities/Entity.php
@@ -10,4 +10,45 @@ class Entity extends BaseEntity {
 
     protected $fillable = ['titulo','area','funcionario','otro','estado'];
 
+    // ORDERNAR
+    public function scopeOrder($query, $order)
+    {
+        switch ($order){
+            case '':
+                $query->orderBy('titulo', 'desc');
+                break;
+
+            case 'tituloAsc':
+                $query->orderBy('titulo', 'asc');
+                break;
+
+            case 'tituloDesc':
+                $query->orderBy('titulo', 'desc');
+                break;
+
+            case 'areaAsc':
+                $query->orderBy('area', 'asc');
+                break;
+
+            case 'areaDesc':
+                $query->orderBy('area', 'desc');
+                break;
+
+            case 'funcionarioAsc':
+                $query->orderBy('funcionario', 'asc');
+                break;
+
+            case 'funcionarioDesc':
+                $query->orderBy('funcionario', 'desc');
+                break;
+
+            case 'otroAsc':
+                $query->orderBy('otro', 'asc');
+                break;
+
+            case 'otroDesc':
+                $query->orderBy('otro', 'desc');
+                break;
+        }
+    }
 }
