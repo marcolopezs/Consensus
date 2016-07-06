8d341dc83f866220d3280106a1e10dd7325511e5
Marco Lopez <mlopez@marostdevelopers.com>
2016-07-06 18:35:53 -0500

    funcion findOrder en entidad

--- app/Repositories/EntityRepo.php
+++ app/Repositories/EntityRepo.php
@@ -1,12 +1,26 @@
 <?php namespace Consensus\Repositories;
 
 use Consensus\Entities\Entity;
+use Illuminate\Http\Request;
 
 class EntityRepo extends BaseRepo {
 
     public function getModel()
     {
         return new Entity();
+    }
+
+    //BUSQUEDA DE REGISTROS POR TITULO Y ESTADO y ORDENARLO POR SELECCION DEL USUARIO
+    public function findOrder(Request $request)
+    {
+        return $this->getModel()
+                    ->titulo($request->get('titulo'))
+                    ->estado($request->get('estado'))
+                    ->area($request->get('area'))
+                    ->funcionario($request->get('funcionario'))
+                    ->otro($request->get('otro'))
+                    ->order($request->get('order'))
+                    ->paginate();
     }
 
 }
