ba039f6364fe52a5e105862764ce01709b9cc5af
Marco Lopez <mlopez@marostdevelopers.com>
2016-07-06 19:15:26 -0500

    funcion findOrder() en Estado

--- app/Repositories/StateRepo.php
+++ app/Repositories/StateRepo.php
@@ -1,11 +1,22 @@
 <?php namespace Consensus\Repositories;
 
 use Consensus\Entities\State;
+use Illuminate\Http\Request;
 
 class StateRepo extends BaseRepo {
 
     public function getModel()
     {
         return new State();
+    }
+
+    //BUSQUEDA DE REGISTROS POR TITULO Y ESTADO y ORDENARLO POR SELECCION DEL USUARIO
+    public function findOrder(Request $request)
+    {
+        return $this->getModel()
+                    ->titulo($request->get('titulo'))
+                    ->estado($request->get('estado'))
+                    ->order($request->get('order'))
+                    ->paginate();
     }
 }
