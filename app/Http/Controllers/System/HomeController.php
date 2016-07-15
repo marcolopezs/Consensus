537a0e204bfecf57f032f7f13e7a0a63d5b734c8
Marco Lopez <mlopez@marostdevelopers.com>
2016-07-15 17:04:17 -0500

    mostrar tipos de expediente en home

--- app/Http/Controllers/System/HomeController.php
+++ app/Http/Controllers/System/HomeController.php
@@ -1,32 +1,39 @@
 <?php namespace Consensus\Http\Controllers\System;
 
+use Carbon\Carbon;
 use Consensus\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Gate;
 
 use Consensus\Repositories\ClienteRepo;
 use Consensus\Repositories\ExpedienteRepo;
+use Consensus\Repositories\ExpedienteTipoRepo;
 use Consensus\Repositories\TareaRepo;
 
 class HomeController extends Controller {
 
     protected $clienteRepo;
     protected $expedienteRepo;
+    protected $expedienteTipoRepo;
+    protected $tareaRepo;
 
     protected $usuario;
     protected $cliente;
-    protected $tareaRepo;
 
     /**
      * HomeController constructor.
      * @param ClienteRepo $clienteRepo
      * @param ExpedienteRepo $expedienteRepo
+     * @param ExpedienteTipoRepo $expedienteTipoRepo
      * @param TareaRepo $tareaRepo
      */
     public function __construct(ClienteRepo $clienteRepo,
-                                ExpedienteRepo $expedienteRepo, TareaRepo $tareaRepo)
+                                ExpedienteRepo $expedienteRepo,
+                                ExpedienteTipoRepo $expedienteTipoRepo,
+                                TareaRepo $tareaRepo)
     {
         $this->clienteRepo = $clienteRepo;
         $this->expedienteRepo = $expedienteRepo;
+        $this->expedienteTipoRepo = $expedienteTipoRepo;
         $this->tareaRepo = $tareaRepo;
 
         if(Gate::allows('cliente')) {
@@ -46,7 +53,9 @@ class HomeController extends Controller {
             $tareasPendientes = $this->tareaRepo->filterHome(0);
             $tareasTerminadas = $this->tareaRepo->filterHome(1);
 
-            return view('system.index', compact('tareasPendientes','tareasTerminadas'));
+            $expedientes_tipo = $this->expedienteTipoRepo->where('estado','1')->get();
+
+            return view('system.index', compact('expedientes_tipo','tareasPendientes','tareasTerminadas'));
         }
     }
 
