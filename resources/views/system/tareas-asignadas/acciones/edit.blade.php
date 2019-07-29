ea03df18652a74be9c227ab4b3bcb1d783856cde
Marco Lopez <mlopez@syscontrollers.com>
2019-07-29 00:59:52 -0500

    inputs dentro de .row en create y edit de acciones de tarea

--- resources/views/system/tareas-asignadas/acciones/edit.blade.php
+++ resources/views/system/tareas-asignadas/acciones/edit.blade.php
@@ -28,40 +28,42 @@
 
                     <div class="form-body">
 
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                {!! Form::label('fecha', 'Fecha', ['class' => 'control-label']) !!}
-                                <div class="input-group input-medium date date-picker" data-date-format="dd/mm/yyyy" data-date-viewmode="years">
-                                    {!! Form::text('fecha', $prin->fecha_accion, ['class' => 'form-control']) !!}
-                                    <span class="input-group-btn"><button class="btn default" type="button"><i class="fa fa-calendar"></i></button></span>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    {!! Form::label('fecha', 'Fecha', ['class' => 'control-label']) !!}
+                                    <div class="input-group input-medium date date-picker" data-date-format="dd/mm/yyyy" data-date-viewmode="years">
+                                        {!! Form::text('fecha', $prin->fecha_accion, ['class' => 'form-control']) !!}
+                                        <span class="input-group-btn"><button class="btn default" type="button"><i class="fa fa-calendar"></i></button></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                {!! Form::label('desde', 'Desde', ['class' => 'control-label']) !!}
-                                <div class="input-group">
-                                    {!! Form::text('desde', null, ['class' => 'form-control timepicker timepicker-24']) !!}
-                                    <span class="input-group-btn"><button class="btn default" type="button"><i class="fa fa-clock-o"></i></button></span>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    {!! Form::label('desde', 'Desde', ['class' => 'control-label']) !!}
+                                    <div class="input-group">
+                                        {!! Form::text('desde', null, ['class' => 'form-control timepicker timepicker-24']) !!}
+                                        <span class="input-group-btn"><button class="btn default" type="button"><i class="fa fa-clock-o"></i></button></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                {!! Form::label('hasta', 'Hasta', ['class' => 'control-label']) !!}
-                                <div class="input-group">
-                                    {!! Form::text('hasta', null, ['class' => 'form-control timepicker timepicker-24']) !!}
-                                    <span class="input-group-btn"><button class="btn default" type="button"><i class="fa fa-clock-o"></i></button></span>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    {!! Form::label('hasta', 'Hasta', ['class' => 'control-label']) !!}
+                                    <div class="input-group">
+                                        {!! Form::text('hasta', null, ['class' => 'form-control timepicker timepicker-24']) !!}
+                                        <span class="input-group-btn"><button class="btn default" type="button"><i class="fa fa-clock-o"></i></button></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                {!! Form::label('descripcion', 'Descripción', ['class' => 'control-label']) !!}
-                                {!! Form::textarea('descripcion', null, ['class' => 'form-control', 'rows' => '3']) !!}
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    {!! Form::label('descripcion', 'Descripción', ['class' => 'control-label']) !!}
+                                    {!! Form::textarea('descripcion', null, ['class' => 'form-control', 'rows' => '3']) !!}
+                                </div>
                             </div>
                         </div>
 
