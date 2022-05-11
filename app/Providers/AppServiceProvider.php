7c41167b2bf96c5f17ad6a9e262ae5e45be5af2c
Marco Lopez <mlopez@syscontrollers.com>
2022-05-11 17:46:58 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment('local')) {
             $this->app->register('Barryvdh\Debugbar\ServiceProvider');
+
+            if(version_compare(PHP_VERSION, '7.2.0', '>=')) {
+                error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+            }
         }
     }
 }
